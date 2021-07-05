9f700651842dd8cd033840df16c7edf487a9e885
jothamgoh <35646888+jothamgoh@users.noreply.github.com>
2021-07-05 23:08:03 +0800

    changed typo in form

--- app/main/forms.py
+++ app/main/forms.py
@@ -15,7 +15,7 @@ class SearchCustomerForm(FlaskForm):
 class RegisterPackageForm(FlaskForm):
     package_name = SelectField(('Package Name'), validators=[DataRequired()], choices=package_choices)
     package_num_total_uses_at_start = IntegerField(('No. times package can be used'), validators=[DataRequired()])
-    package_uses_left_when_keyed = IntegerField(('No. times package has already been used'), validators=[InputRequired()])
+    package_num_used_when_keyed = IntegerField(('No. times package has already been used'), validators=[InputRequired()])
     package_price_paid = DecimalField(('Price Paid for Package (SGD)'), validators=[InputRequired()], places=2)
     submit = SubmitField(('Register package for customer'))
 
@@ -25,7 +25,7 @@ class PortCustomerAndPackageForm(FlaskForm):
     name = StringField(('Customer Name'))
     package_name = SelectField(('Package Name'), validators=[DataRequired()], choices=package_choices)
     package_num_total_uses_at_start = IntegerField(('No. times package can be used'), validators=[DataRequired()])
-    package_uses_left_when_keyed = IntegerField(('No. times package has already been used'), validators=[InputRequired()])
+    package_num_used_when_keyed = IntegerField(('No. times package has already been used'), validators=[InputRequired()])
     package_price_paid = DecimalField(('Price Paid for Package (SGD)'), places=2)
     created_at = DateField('Date Package Created (YYYY-MM-DD e.g. 2021-01-30)', format='%Y-%m-%d')
     submit = SubmitField(('Port new package'))
